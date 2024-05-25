5b0f56d599a6280c4de96a2f144773f4dd34ddcc
Yiğit Kaan Duran <160050842+duranyi46@users.noreply.github.com>
2024-05-25 18:00:59 +0300

    Add files via upload
    end of q1

--- Quest1.cpp
+++ Quest1.cpp
@@ -1,72 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
-enum {SUCCESS,FAIL};
+enum { SUCCESS, FAIL };
 
-int main()
-{
-	int studentNumber[8] = {9,0,2,3,0,7,6,7}; /* Creating array, its size and files even.txt and odd.txt */
-	int size = sizeof(studentNumber) / 4;
-	FILE *fptr1, *fptr2;
-	char filename1[]= "even.txt";
-	char filename2[]= "odd.txt";
-	int reval = SUCCESS;
-	
-	if ((fptr1 = fopen(filename1, "w")) == NULL) { /* In case even.txt cannot be opened */
-	printf("Can't open %s\n",filename1);
-	reval = FAIL;
-	} else if ((fptr2 = fopen(filename2, "w")) == NULL) { /* In case odd.txt cannot be opened */
-	printf("Can't open %s\n",filename2);
-	reval = FAIL;
-	} else { /* Both text files are created and opened. Writing even and odd integers corresponding files */
-	for (int i = 0; i < size; i++) {
-		if (studentNumber[i] % 2 == 0) {
-			fprintf(fptr1, "%i ",studentNumber[i]);
-		} else {
-			fprintf(fptr2, "%i ",studentNumber[i]);
-		}
-	}
-	 }
-	fclose(fptr1); fclose(fptr2); /* End of part a */
-	
-	if ((fptr1 = fopen(filename1, "r+")) == NULL) { /* In case even.txt cannot be opened */
-	printf("Can't open %s\n",filename1);
-	reval = FAIL;
-	} else if ((fptr2 = fopen(filename2, "r+")) == NULL) { /* In case odd.txt cannot be opened */
-	printf("Can't open %s\n",filename2);
-	reval = FAIL;
-	} else { /* Both text files are created and opened. Reading integers and calculating sums */
-	int integer, even_sum = 0, odd_sum = 0; /* Defining variables for calculating sums */
-	while (fscanf(fptr1, "%i", &integer) == 1) { /* Calculating even integers sum */
-		even_sum += integer;
-	}
-	while (fscanf(fptr2, "%i", &integer) == 1) { /* Calculating odd integers sum */
-		odd_sum += integer;
-	}
-	fprintf(fptr1, "%i",even_sum); /* Appending sum of even and odd integers at the end of the first lines of each file */
-	fprintf(fptr2, "%i",odd_sum);
-	 }
-	fclose(fptr1); fclose(fptr2); /* End of part b */
-	
-	/* Open text.txt file */
-	FILE *fptr3;
-	fptr3 = fopen("text.txt","r");
-	int letter,j; int ascii_values[1050];
-	
-	if ((fptr3 = fopen("text.txt", "r")) == NULL) { /* In case text.txt cannot be opened */
-		printf("Can't open %s\n",fptr3);
-	} else {
-		while((letter = getc(fptr3)) != EOF) {
-			if (isalpha(letter)) {
-				ascii_values[j++] = letter;
-			}
-		}
-		ascii_values[j] = '/0';
-	 	}
+int main() {
+    int studentNumber[8] = {9, 0, 2, 3, 0, 7, 6, 7}; /* Creating array, its size and files even.txt and odd.txt */
+    int size = sizeof(studentNumber) / sizeof(studentNumber[0]);
+    FILE *fptr1, *fptr2, *fptr3;
+    char filename1[] = "even.txt";
+    char filename2[] = "odd.txt";
+    int reval = SUCCESS;
 
-	fclose(fptr3);
-	return reval;
-	}
-	
-	
-	
+    if ((fptr1 = fopen(filename1, "w")) == NULL) { /* In case even.txt cannot be opened */
+        printf("Can't open %s\n", filename1);
+        return FAIL;
+    } else if ((fptr2 = fopen(filename2, "w")) == NULL) { /* In case odd.txt cannot be opened */
+        printf("Can't open %s\n", filename2);
+        fclose(fptr1);
+        return FAIL;
+    } else { /* Both text files are created and opened. Writing even and odd integers corresponding files */
+        for (int i = 0; i < size; i++) {
+            if (studentNumber[i] % 2 == 0) {
+                fprintf(fptr1, "%i ", studentNumber[i]);
+            } else {
+                fprintf(fptr2, "%i ", studentNumber[i]);
+            }
+        }
+    }
+    fclose(fptr1);
+    fclose(fptr2); /* End of part a */
+
+    if ((fptr1 = fopen(filename1, "r+")) == NULL) { /* In case even.txt cannot be opened */
+        printf("Can't open %s\n", filename1);
+        return FAIL;
+    } else if ((fptr2 = fopen(filename2, "r+")) == NULL) { /* In case odd.txt cannot be opened */
+        printf("Can't open %s\n", filename2);
+        fclose(fptr1);
+        return FAIL;
+    } else { /* Both text files are created and opened. Reading integers and calculating sums */
+        int integer, even_sum = 0, odd_sum = 0; /* Defining variables for calculating sums */
+        while (fscanf(fptr1, "%i", &integer) == 1) { /* Calculating even integers sum */
+            even_sum += integer;
+        }
+        while (fscanf(fptr2, "%i", &integer) == 1) { /* Calculating odd integers sum */
+            odd_sum += integer;
+        }
+        fprintf(fptr1, " %i", even_sum); /* Appending sum of even and odd integers at the end of the first lines of each file */
+        fprintf(fptr2, " %i", odd_sum);
+    }
+    fclose(fptr1);
+    fclose(fptr2); /* End of part b */
+
+    fptr3 = fopen("text.txt", "r");
+    if (fptr3 == NULL) { /* In case text.txt cannot be opened */
+        printf("Can't open text.txt\n");
+        return FAIL;
+    } else { /* text.txt opened */
+        int letter, count = 0;
+        int *ascii_values = NULL;
+        while ((letter = getc(fptr3)) != EOF) {
+            if (isalpha(letter)) { /* checking whether it is a letter or not */
+                ascii_values = (int *)realloc(ascii_values, (count + 1) * sizeof(int));
+                if (ascii_values == NULL) {
+                    printf("Memory allocation failed.\n");
+                    return FAIL;
+                }
+                ascii_values[count++] = letter; /* if it is a letter get its ascii value in array */
+            }
+        }
+        fclose(fptr3); /* closing text.txt */
+
+        if ((fptr1 = fopen(filename1, "a")) == NULL) { /* In case even.txt cannot be opened */
+            printf("Can't open %s\n", filename1);
+            free(ascii_values);
+            return FAIL;
+        } else if ((fptr2 = fopen(filename2, "a")) == NULL) { /* In case odd.txt cannot be opened */
+            printf("Can't open %s\n", filename2);
+            free(ascii_values);
+            return FAIL;
+        } else { /* Both text files are created and opened. Writing even and odd ascii values corresponding files */
+            fprintf(fptr1, "\n");
+            fprintf(fptr2, "\n");
+            for (int i = 0; i < count; i++) {
+                if (ascii_values[i] % 2 == 0) {
+                    fprintf(fptr1, "%i ", ascii_values[i]);
+                } else {
+                    fprintf(fptr2, "%i ", ascii_values[i]);
+                }
+            }
+            fclose(fptr1);
+            fclose(fptr2);
+        }
+        free(ascii_values);
+    }
+
+    return reval;
+}
+
